b92461a4c397e49ce13345bcedabc9999a168a77
chiptopher <chrisboyerdev@gmail.com>
2018-07-14 17:42:07 -0400

    Update setup.py to include long description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import guet
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='guet',
     version=guet.__version__,
     description='Enable contribution tracking when pair programming',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/chiptopher/guet',
     keywords='pair programming',
     entry_points={
