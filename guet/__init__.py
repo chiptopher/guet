46b38c87f25fbf296d67e125e43b55fc91ac38a4
chiptopher <chrisboyerdev@gmail.com>
2019-10-30 22:47:18 -0400

    Upgrade version number to 2.0.0

--- guet/__init__.py
+++ guet/__init__.py
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = '1.1.0'
+__version__ = '2.0.0'
 
 from guet.commit import PostCommitManager
