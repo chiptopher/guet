fdf9b56c455fd6b50e7e6ce94ba2c6eb0325eeed
chiptopher <chrisboyerdev@gmail.com>
2019-11-03 12:33:41 -0500

    Amending a commit will remove already present Co-authored messages

--- test/git/test_edit_commit_msg.py
+++ test/git/test_edit_commit_msg.py
@@ -8,8 +8,11 @@ from guet.git.edit_commit_msg import edit_commit_msg
 class TestEditCommitMsg(unittest.TestCase):
 
     @patch('builtins.open', new_callable=mock_open())
-    def test_appends_given_text_to_file(self, mock_open):
-        git_path = 'path/to/.git'
-        edit_commit_msg(git_path, ['line1\n', 'line2\n'])
-        mock_open.assert_called_with(join(git_path, 'COMMIT_EDITMSG'), 'a')
-        mock_open.return_value.writelines.assert_called_with(['line1\n', 'line2\n'])
+    def test_writes_text_to_edit_commit_msg_file(self,
+                                                 mock_open):
+        edit_commit_msg('path/to/.git', [
+            'Text\n'
+        ])
+
+        mock_open.assert_called_with(join('path/to/.git', 'COMMIT_EDITMSG'), 'w')
+        mock_open.return_value.writelines.assert_called_with(['Text\n'])
