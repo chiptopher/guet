ffeafc96b98234201b024607c5d8a53102dcf6cc
chiptopher <chrisboyerdev@gmail.com>
2019-11-03 13:04:07 -0500

    Build commands based of map rather than looping through all subclasses of Command

--- test/test_factory.py
+++ test/test_factory.py
@@ -1,8 +1,6 @@
-
 import unittest
 
 from guet.commands.command import Command
-from guet.commands.help import HelpCommand
 from guet.factory import CommandFactory
 
 
@@ -14,22 +12,23 @@ class MockCommand(Command):
         return False
 
 
+class NotMockCommand(Command):
+    def execute(self):
+        pass
+
+    def validate(self, arguments: list):
+        return False
+
+
 class TestCommandFactory(unittest.TestCase):
 
-    def test_create_should_iterate_through_all_commands_and_return_the_one_that_matches(self):
+    def test_returns_command_from_builder_method_that_matches(self):
+        builder_map = dict()
+        builder_map['command'] = MockCommand
+        builder_map['not-command'] = NotMockCommand
 
-        class MockCommandA(MockCommand):
-            def validate(self, arguments: list):
-                return True
-
-        args = []
-        command_factory = CommandFactory()
-        actual_command = command_factory.create(args, MockCommand.__subclasses__())
-        self.assertEqual(MockCommandA, actual_command.__class__)
-
-    def test_create_returns_help_command_when_the_arguement_is_not_valid_using_actual_subclasses(self):
-
-        args = []
-        command_factory = CommandFactory()
-        resulting_command = command_factory.create(args)
-        self.assertEqual(HelpCommand, resulting_command.__class__)
+        command_factory = CommandFactory(builder_map)
+        args = ['command']
+        result = command_factory.create(args)
+        self.assertEqual(MockCommand, type(result))
+        self.assertEqual(args, result._args)
