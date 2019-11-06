245f58964f5d8da7d1fc1432ef32719caaeb1956
chiptopher <chrisboyerdev@gmail.com>
2019-11-06 08:35:46 -0500

    Add modules for getting and settings config file data

---
+++ test/config/test_get_config.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+
+from guet.config.get_config import get_config
+
+
+@patch('guet.config.get_config.read_lines')
+class TestGetConfig(unittest.TestCase):
+    def test_loads_settings_from_config_file_lines(self, mock_read_lines):
+        mock_read_lines.return_value = [
+            'pairReset=False\n'
+        ]
+        settings = get_config()
+        self.assertEqual(False, settings.read('pairReset'))
---
+++ test/config/test_set_config.py
@@ -0,0 +1,21 @@
+import unittest
+from os.path import join
+from unittest.mock import patch
+
+from guet import constants
+from guet.config import configuration_directory
+from guet.config.set_config import set_config
+from guet.settings.settings import Settings
+
+
+@patch('guet.config.set_config.write_lines')
+class TestSetConfig(unittest.TestCase):
+
+    def test_converts_settings_to_lines_and_writes_to_config_file(self,
+                                                                  mock_write_lines):
+        settings = Settings()
+        settings.load([
+            'pairReset=False\n'
+        ])
+        set_config(settings)
+        mock_write_lines.assert_called_with(join(configuration_directory, constants.CONFIG), ['pairReset=False\n'])
