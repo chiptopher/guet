44d236581a34e45a4d46498a832305788b7cde5f
chiptopher <chrisboyerdev@gmail.com>
2019-11-09 11:18:54 -0500

    Move SetCoommitters to Command2

--- guet/commands/setcommitters.py
+++ guet/commands/setcommitters.py
@@ -1,37 +1,15 @@
-"""
-Copyright 2018 Christopher M. Boyer
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-from typing import List
-
+from guet.commands.command import Command2
 from guet.config.committer import filter_committers_with_initials, Committer
 from guet.config.get_committers import get_committers
 from guet.config.set_author import set_committer_as_author
 from guet.config.set_current_committers import set_current_committers as set_committers
-from .command import Command
 
 
-class SetCommittersCommand(Command):
-    _REQUIRED_ARGS_IN_CORRECT_ORDER = ['set']
+class SetCommittersCommand(Command2):
 
-    def __init__(self,
-                 args):
-        super().__init__(args)
-
-    def execute(self):
+    def execute_hook(self) -> None:
         committers = get_committers()
-        committer_initials = self._args[1:]
+        committer_initials = self.args
         committers_to_set = filter_committers_with_initials(committers, committer_initials)
 
         correct_number_of_committers_present = len(committers_to_set) is len(committer_initials)
@@ -52,6 +30,5 @@ class SetCommittersCommand(Command):
         pass
 
     @classmethod
-    def get_short_help_message(cls):
+    def help_short(cls) -> str:
         return 'Set the current committers'
-
