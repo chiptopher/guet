fee8e58dc621e672f63ec44ad416ff134994dba0
chiptopher <chrisboyerdev@gmail.com>
2019-11-09 20:21:30 -0500

    Exit on error

--- test/util/test_errors.py
+++ test/util/test_errors.py
@@ -4,6 +4,7 @@ from unittest.mock import patch, Mock
 from guet.util.errors import log_on_error
 
 
+@patch('builtins.exit')
 @patch('builtins.print')
 @patch('guet.util.errors.set_errors')
 @patch('traceback.format_exc')
@@ -11,7 +12,8 @@ class TestErrorWrapper(unittest.TestCase):
     def test_writes_to_error_file_when_something_goes_wrong(self,
                                                             mock_format_exc,
                                                             mock_set_errors,
-                                                            mock_print):
+                                                            mock_print,
+                                                            mock_exit):
         @log_on_error
         def func():
             raise ValueError
@@ -27,7 +29,8 @@ class TestErrorWrapper(unittest.TestCase):
     def test_says_an_error_occurred(self,
                                     mock_format_exc,
                                     mock_set_errors,
-                                    mock_print):
+                                    mock_print,
+                                    mock_exit):
         @log_on_error
         def func():
             raise ValueError
@@ -39,3 +42,20 @@ class TestErrorWrapper(unittest.TestCase):
         func()
 
         mock_print.assert_called_with('An error has occurred, please refer to error logs for more information\n')
+
+    def test_exits_one_on_error(self,
+                                    mock_format_exc,
+                                    mock_set_errors,
+                                    mock_print,
+                                    mock_exit):
+        @log_on_error
+        def func():
+            raise ValueError
+
+        mock_format_exc.return_value = [
+            'line1\n'
+        ]
+
+        func()
+
+        mock_exit.assert_called_with(1)
