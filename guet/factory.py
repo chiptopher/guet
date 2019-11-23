e0b86e63d1b2a732f05117d3e9b16fc81ec558f3
chiptopher <chrisboyerdev@gmail.com>
2019-11-23 17:08:08 -0500

    Use the system configured settings if init has already been ran

--- guet/factory.py
+++ guet/factory.py
@@ -1,6 +1,8 @@
 from guet.commands import HelpCommand
 from guet.commands.command import Command
 from guet.settings.settings import Settings
+from guet.config.get_config import get_config
+from guet.config.already_initialized import already_initialized
 
 
 class CommandFactory:
@@ -8,9 +10,17 @@ class CommandFactory:
         self.command_builder_map = command_builder_map
 
     def create(self, args: list) -> Command:
+        result = None
+        if already_initialized():
+            result = self._create_with_settings(args, get_config())
+        else:
+            result = self._create_with_settings(args, Settings())
+        return result
+
+    def _create_with_settings(self, args: list, settings: Settings) -> Command:
         if len(args) > 0:
             command_arg = args[0]
-            initialized_command = self.command_builder_map[command_arg](args, Settings())
-            return initialized_command
+            command = self.command_builder_map[command_arg](args, settings)
+            return command
         else:
             return HelpCommand(args, Settings(), self.command_builder_map)
--- test/test_factory.py
+++ test/test_factory.py
@@ -1,5 +1,5 @@
 import unittest
-
+from unittest.mock import patch
 from guet.commands import HelpCommand
 from guet.commands.command import Command
 from guet.factory import CommandFactory
@@ -30,9 +30,11 @@ class NotMockCommand(Command):
         pass
 
 
+@patch('guet.factory.already_initialized', return_value=True)
+@patch('guet.factory.get_config')
 class TestCommandFactory(unittest.TestCase):
-
-    def test_returns_command_from_builder_method_that_matches(self):
+    def test_returns_command_from_builder_method_that_matches(self, mock_get_settings,
+                                                              mock_already_init):
         builder_map = dict()
         builder_map['command'] = MockCommand
         builder_map['not-command'] = NotMockCommand
@@ -42,14 +44,42 @@ class TestCommandFactory(unittest.TestCase):
         result = command_factory.create(args)
         self.assertEqual(MockCommand, type(result))
 
-    def test_returns_help_command_if_no_command_provided(self):
+    def test_returns_command_with_settings_from_settings_file(self, mock_get_settings,
+                                                              mock_already_init):
+        expected_settings = Settings()
+        mock_get_settings.return_value = expected_settings
+        builder_map = dict()
+        builder_map['command'] = MockCommand
+        builder_map['not-command'] = NotMockCommand
+
+        command_factory = CommandFactory(builder_map)
+        args = ['command']
+        result = command_factory.create(args)
+        self.assertEqual(expected_settings, result.settings)
+
+    def test_uses_new_settings_with_default_values_if_initialization_hasnt_been_ran(
+            self, mock_get_settings, mock_already_init):
+        mock_already_init.return_value = False
+        returned_settings = Settings()
+        mock_get_settings.return_value = returned_settings
+        builder_map = dict()
+        builder_map['command'] = MockCommand
+
+        command_factory = CommandFactory(builder_map)
+        args = ['command']
+        result = command_factory.create(args)
+        self.assertNotEqual(returned_settings, result.settings)
+
+    def test_returns_help_command_if_no_command_provided(self, mock_get_settings,
+                                                         mock_already_init):
         command_factory = CommandFactory(dict())
         result = command_factory.create([])
         self.assertEqual(HelpCommand, type(result))
 
-    def test_help_command_has_command_builder_map(self):
+    def test_help_command_has_command_builder_map(self, mock_get_settings, mock_already_init):
+        mock_get_settings.return_value = Settings()
         command_builder_map = dict()
-        command_builder_map['command'] = lambda args: Command(args, Settings())
+        command_builder_map['command'] = lambda args, settings: Command(args, settings)
         command_factory = CommandFactory(command_builder_map)
         result = command_factory.create([])
         self.assertEqual(command_builder_map, result.command_builder_map)
