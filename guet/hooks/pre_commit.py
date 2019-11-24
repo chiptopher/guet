06d534cccabc2c38259b8bc47570ee4a09f8f516
chiptopher <chrisboyerdev@gmail.com>
2019-11-24 17:20:58 -0500

    If pairReset is set to false don't check if last pair set was over 24 hours ago

--- guet/hooks/pre_commit.py
+++ guet/hooks/pre_commit.py
@@ -1,10 +1,19 @@
+from guet.config.get_config import get_config
 from guet.config.most_recent_committers_set import most_recent_committers_set
 from guet.currentmillis import current_millis
 from guet.util.errors import log_on_error
 
 
 @log_on_error
-def pre_commit():
+def pre_commit():    
+    _check_within_timeframe_if_enabled()
+
+def _check_within_timeframe_if_enabled():
+    settings = get_config()
+    if settings.read('pairReset'):
+        _fail_if_past_valid_timeframe()
+
+def _fail_if_past_valid_timeframe():
     now = current_millis()
     twenty_four_hours = 86400000
     twenty_four_hours_ago = now - twenty_four_hours
@@ -13,5 +22,3 @@ def pre_commit():
         print(("\nYou have not reset pairs in over twenty four hours!\n" +
                "Please reset your pairs by using guet set and including your pairs' initials\n"))
         exit(1)
-    else:
-        exit(0)
--- test/hooks/test_pre_commit.py
+++ test/hooks/test_pre_commit.py
@@ -4,16 +4,16 @@ from unittest.mock import patch
 
 from guet.currentmillis import current_millis
 from guet.hooks.pre_commit import pre_commit
+from guet.settings.settings import Settings
 
 
+@patch('guet.hooks.pre_commit.get_config', return_value=Settings())
+@patch('guet.hooks.pre_commit.most_recent_committers_set')
+@patch('builtins.print')
+@patch('builtins.exit')
 class TestPreCommit(unittest.TestCase):
-    @patch('guet.hooks.pre_commit.most_recent_committers_set')
-    @patch('builtins.print')
-    @patch('builtins.exit')
-    def test_manage_checks_for_most_recent_pair_set_and_exits_1_if_it_is_over_24_hours(self,
-                                                                                       mock_exit,
-                                                                                       mock_print,
-                                                                                       mock_most_recent_committers_set):
+    def test_manage_checks_for_most_recent_pair_set_and_exits_1_if_it_is_over_24_hours(
+            self, mock_exit, mock_print, mock_most_recent_committers_set, mock_get_config):
         twenty_four_hours = 86400000
         now = current_millis()
         mock_most_recent_committers_set.return_value = now - 100 - twenty_four_hours
@@ -22,18 +22,27 @@ class TestPreCommit(unittest.TestCase):
 
         mock_exit.assert_called_once_with(1)
         mock_print.assert_called_with(
-            "\nYou have not reset pairs in over twenty four hours!\nPlease reset your pairs by using guet set and including your pairs' initials\n")
+            "\nYou have not reset pairs in over twenty four hours!\nPlease reset your pairs by using guet set and including your pairs' initials\n"
+        )
 
-    @patch('guet.hooks.pre_commit.most_recent_committers_set')
-    @patch('builtins.print')
-    @patch('builtins.exit')
-    def test_manage_checks_for_most_recent_pair_set_and_exits_0_if_it_is_under_24_hours(self,
-                                                                                        mock_exit,
-                                                                                        mock_print,
-                                                                                        mock_most_recent_committers_set):
+    def test_manage_checks_for_most_recent_pair_set_and_exits_0_if_it_is_under_24_hours(
+            self, mock_exit, mock_print, mock_most_recent_committers_set, mock_get_config):
         ten_hours = 36000000
         now = round((datetime.datetime.utcnow().timestamp() - 1) * 1000)
         mock_most_recent_committers_set.return_value = now - ten_hours
         pre_commit()
 
-        mock_exit.assert_called_once_with(0)
+        mock_exit.assert_not_called()
+
+    def test_wont_error_when_time_is_over_24_hours_if_pair_reset_is_disabled(
+            self, mock_exit, mock_print, mock_most_recent_committers_set, mock_get_config):
+        twenty_four_hours = 86400000
+        now = current_millis()
+        mock_most_recent_committers_set.return_value = now - 100 - twenty_four_hours
+        pair_reset_disabled_settings = Settings()
+        pair_reset_disabled_settings.set('pairReset', 'False')
+        mock_get_config.return_value = pair_reset_disabled_settings
+
+        pre_commit()
+
+        mock_exit.assert_not_called()
