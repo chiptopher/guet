5b785fcec8232e248b423521ec31f13933c9d408
chiptopher <chrisboyerdev@gmail.com>
2020-01-01 11:35:39 -0500

    Help decorator can be configured to have no args not cause a help message

--- guet/commands/help_decorator.py
+++ guet/commands/help_decorator.py
@@ -10,12 +10,14 @@ from guet.settings.settings import Settings
 
 class HelpDecorator(CommandFactoryDecorator):
 
-    def __init__(self, decorated: CommandFactoryMethod, help_message: str):
+    def __init__(self, decorated: CommandFactoryMethod, help_message: str, no_args_valid=False):
         super().__init__(decorated)
+        self._no_args_valid = no_args_valid
         self._help_message = help_message
 
     def build(self, args: List[str], settings: Settings) -> Command:
-        if len(args) is 1 or '-h' in args or '--help' in args:
+        should_print_help_because_of_no_args = len(args) is 1 and not self._no_args_valid
+        if should_print_help_because_of_no_args or '-h' in args or '--help' in args:
             return StrategyCommand(HelpMessageStrategy(self._help_message))
         else:
             return self.decorated.build(args, settings)
