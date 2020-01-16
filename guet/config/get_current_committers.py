f3afa337f7b1757697b27fbfba40fdc5404ed3f4
chiptopher <chrisboyerdev@gmail.com>
2020-01-15 23:38:09 -0500

    Refactor get_current_committers to use builtins for filtering and finding in lists

--- guet/config/get_current_committers.py
+++ guet/config/get_current_committers.py
@@ -6,18 +6,24 @@ from guet.config import CONFIGURATION_DIRECTORY
 from guet.config.committer import Committer, filter_committers_with_initials
 from guet.config.get_committers import get_committers
 
+POSITION_OF_LAST_ELEMENT = -1
+
+
+def _get_committer_with_initials(committers: List[Committer], initials: str) -> Committer:
+    return next(filter(lambda committer: committer.initials == initials, committers))
+
+
+def _committers_in_order_of_initials(committers: List[Committer],
+                                     committer_initials: List[str]) -> List[Committer]:
+    return list(map(lambda initials: _get_committer_with_initials(committers, initials), committer_initials))
+
 
 def get_current_committers() -> List[Committer]:
     set_committers_file = open(join(CONFIGURATION_DIRECTORY, constants.COMMITTERS_SET), 'r')
     line = set_committers_file.readline()
     set_committers_file.close()
     committer_initials = line.rstrip().split(',')
-    del committer_initials[-1]
+    del committer_initials[POSITION_OF_LAST_ELEMENT]
     committers = get_committers()
     committers_with_initials = filter_committers_with_initials(committers, committer_initials)
-    final = []
-    for initials in committer_initials:
-        for committer in committers_with_initials:
-            if committer.initials == initials:
-                final.append(committer)
-    return final
+    return _committers_in_order_of_initials(committers_with_initials, committer_initials)
