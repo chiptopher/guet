d039a0c3ca1252d17d160923040ec9ea094a7ba1
chiptopher <chrisboyerdev@gmail.com>
2020-01-26 12:42:59 -0500

    Require being in a git folder to run 'guet set'

--- e2e/test_commit.py
+++ e2e/test_commit.py
@@ -55,11 +55,14 @@ class TestCommit(DockerTest):
 
     def test_wont_allow_commits_in_guet_repo_if_pairs_havent_been_set_in_that_repo(self):
         self.guet_init()
+        self.add_command('mkdir test1')
+        self.change_directory('test1')
         self.guet_add('initials', 'name', 'email@localhost')
         self.guet_add('initials2', 'name2', 'email2@localhost')
         self.guet_set(['initials', 'initials2'])
-        self.add_command('mkdir test')
-        self.change_directory('test')
+        self.change_directory('..')
+        self.add_command('mkdir test2')
+        self.change_directory('test2')
         self.git_init()
         self.guet_start()
         self.add_file('A')
@@ -68,15 +71,18 @@ class TestCommit(DockerTest):
 
         self.execute()
 
-        self.assert_text_in_logs(1, 'You must set your pairs before you can commit.')
+        self.assert_text_in_logs(2, 'You must set your pairs before you can commit.')
 
     def test_can_make_committs_in_multiple_repos_with_committers_set(self):
         self.guet_init()
+        self.add_command('mkdir test1')
+        self.change_directory('test1')
         self.guet_add('initials', 'name', 'email@localhost')
         self.guet_add('initials2', 'name2', 'email2@localhost')
         self.guet_set(['initials', 'initials2'])
-        self.add_command('mkdir test')
-        self.change_directory('test')
+        self.change_directory('..')
+        self.add_command('mkdir test2')
+        self.change_directory('test2')
         self.git_init()
         self.guet_start()
         self.guet_set(['initials', 'initials2'])
@@ -87,5 +93,5 @@ class TestCommit(DockerTest):
 
         self.execute()
 
-        self.assert_text_in_logs(10, '    Co-authored-by: name <email@localhost>')
-        self.assert_text_in_logs(11, '    Co-authored-by: name2 <email2@localhost>')
+        self.assert_text_in_logs(11, '    Co-authored-by: name <email@localhost>')
+        self.assert_text_in_logs(12, '    Co-authored-by: name2 <email2@localhost>')
--- e2e/test_get.py
+++ e2e/test_get.py
@@ -4,6 +4,7 @@ from e2e import DockerTest
 class TestGet(DockerTest):
     def test_get_current_prints_currently_set_committers(self):
         self.guet_init()
+        self.git_init()
         self.guet_add('initials1', 'name1', 'email1')
         self.guet_add('initials2', 'name2', 'email2')
         self.guet_set(['initials1', 'initials2'])
@@ -12,15 +13,14 @@ class TestGet(DockerTest):
 
         self.execute()
 
-        self.assert_text_in_logs(0, 'Currently set committers')
-        self.assert_text_in_logs(1, 'initials1 - name1 <email1>')
-        self.assert_text_in_logs(2, 'initials2 - name2 <email2>')
+        self.assert_text_in_logs(1, 'Currently set committers')
+        self.assert_text_in_logs(2, 'initials1 - name1 <email1>')
+        self.assert_text_in_logs(3, 'initials2 - name2 <email2>')
 
     def test_get_committers_prints_all_committers_on_the_system(self):
         self.guet_init()
         self.guet_add('initials1', 'name1', 'email1')
         self.guet_add('initials2', 'name2', 'email2')
-        self.guet_set(['initials1', 'initials2'])
         self.guet_get_committers()
         self.save_file_content('.guet/errors')
 
