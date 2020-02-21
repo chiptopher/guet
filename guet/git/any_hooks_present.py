2486a1006d3318e9c855d2deeac9f56ac147e1ed
chiptopher <chrisboyerdev@gmail.com>
2020-02-21 09:15:27 -0500

    Remove unused git code

--- guet/git/any_hooks_present.py
+++
@@ -1,9 +0,0 @@
-from guet.git.hook_present import hook_present
-
-
-def any_hooks_present(git_path: str):
-    pre_commit_present = hook_present(git_path, 'pre-commit')
-    post_commit_present = hook_present(git_path, 'post-commit')
-    commit_msg_present = hook_present(git_path, 'commit-msg')
-
-    return pre_commit_present or post_commit_present or commit_msg_present
