a1e825da9ddf5fee9a695ba338254643585a45d9
chiptopher <chrisboyerdev@gmail.com>
2020-02-26 08:09:42 -0500

    Have context load Git and add as an author observer

--- test/context/test_context.py
+++ test/context/test_context.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 from guet.config.committer import Committer
 from guet.context.author_observer import AuthorObserver
@@ -7,10 +7,11 @@ from guet.context.context import Context
 from test.context.errors import InvalidCommittersError
 
 
+@patch('guet.context.context.Git')
 class TestContext(TestCase):
 
-    def test_set_committers_notifies_author_observers_that_committer_in_first_position_is_author(self):
-        context = Context()
+    def test_set_committers_notifies_author_observers_that_committer_in_first_position_is_author(self, mock_git):
+        context = Context('current/working/directory')
 
         committer1 = Committer(name='name1', email='email1', initials='initials1')
         committer2 = Committer(name='name2', email='email2', initials='initials2')
@@ -22,8 +23,8 @@ class TestContext(TestCase):
 
         observer.notify_of_author.assert_called_with(committer1)
 
-    def test_set_committers_raises_exception_when_given_an_empty_list(self):
-        context = Context()
+    def test_set_committers_raises_exception_when_given_an_empty_list(self, mock_git):
+        context = Context('current/working/directory/')
 
         observer: AuthorObserver = Mock()
         context.add_author_observer(observer)
@@ -33,3 +34,14 @@ class TestContext(TestCase):
             self.fail('Should raise exception')
         except InvalidCommittersError:
             pass
+
+    def test_init_loads_git_from_project_root_path_plus_git_directory(self, mock_git):
+        Context('path/to/project/root/')
+        mock_git.assert_called_with('path/to/project/root/.git')
+
+    def test_loads_author_observers(self, mock_git):
+        context = Context('path/to/project/root/')
+
+        expected_author_observers = [mock_git.return_value]
+
+        self.assertListEqual(expected_author_observers, context.author_observers)
