807b74dfd0b2bf29f8c39298f9a969d1be9759bd
chiptopher <chrisboyerdev@gmail.com>
2020-02-26 18:33:33 -0500

    Load Committers and add as an set observer on Context init

--- guet/context/context.py
+++ guet/context/context.py
@@ -2,6 +2,7 @@ from os.path import join
 from typing import List
 
 from guet.config.committer import Committer
+from guet.config.committers import Committers
 from guet.context.set_committers_observable import SetCommittersObservable
 from guet.git.git import Git
 from guet.context.errors import InvalidCommittersError
@@ -11,7 +12,9 @@ class Context(SetCommittersObservable):
     def __init__(self, project_root_directory: str):
         super().__init__()
         git = Git(join(project_root_directory, '.git'))
+        committers = Committers()
         self.add_set_committer_observer(git)
+        self.add_set_committer_observer(committers)
 
     def set_committers(self, committers: List[Committer]):
         if len(committers) > 0:
