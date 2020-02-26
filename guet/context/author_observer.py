f04a1243583b33538ab393c9bb484ed918f98597
chiptopher <chrisboyerdev@gmail.com>
2020-02-26 07:21:26 -0500

    Add author observers and observables

---
+++ guet/context/author_observer.py
@@ -0,0 +1,10 @@
+from guet.config.committer import Committer
+
+
+class AuthorObserver:
+
+    def __init__(self):
+        pass
+
+    def notify_of_author(self, new_author: Committer):
+        pass
