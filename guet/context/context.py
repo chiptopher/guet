0eb9b1586aa200a462103482f38236884bbe8f5a
chiptopher <chrisboyerdev@gmail.com>
2020-02-26 08:09:42 -0500

    Have context load Git and add as an author observer

--- guet/context/context.py
+++ guet/context/context.py
@@ -1,13 +1,17 @@
+from os.path import join
 from typing import List
 
 from guet.config.committer import Committer
 from guet.context.author_observable import AuthorObservable
+from guet.git.git import Git
 from test.context.errors import InvalidCommittersError
 
 
 class Context(AuthorObservable):
-    def __int__(self):
-        pass
+    def __init__(self, project_root_directory: str):
+        super().__init__()
+        git = Git(join(project_root_directory, '.git'))
+        self.add_author_observer(git)
 
     def set_committers(self, committers: List[Committer]):
         if len(committers) > 0:
