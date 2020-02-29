09e2afc89d5f96c28c6f304c2c820fc29cbccfc8
chiptopher <chrisboyerdev@gmail.com>
2020-02-29 08:35:37 -0500

    Use Context to update committers in post-commit hook

--- guet/hooks/post_commit.py
+++ guet/hooks/post_commit.py
@@ -1,24 +1,17 @@
 from os import getcwd
-from os.path import join
 from typing import List
 
 from guet.config.committer import Committer
 from guet.config.get_current_committers import get_current_committers
-from guet.config.set_author import set_committer_as_author
-from guet.config.set_current_committers import set_current_committers
-from guet.git.author import Author
-from guet.git.git import Git
+from guet.context.context import Context
 from guet.util.errors import log_on_error
 
 
 @log_on_error
 def post_commit():
     committers = _rotate_fist_commiter_to_last_committer(get_current_committers())
-    set_committer_as_author(committers[0])
-    set_current_committers(committers)
-    git = Git(join(getcwd(), '.git'))
-    new_author = Author(name=committers[0].name, email=committers[0].email)
-    git.author = new_author
+    context = Context(getcwd())
+    context.set_committers(committers)
 
 
 def _rotate_fist_commiter_to_last_committer(committers: List[Committer]):
--- test/hooks/test_post_commit.py
+++ test/hooks/test_post_commit.py
@@ -2,56 +2,22 @@ import unittest
 from unittest.mock import patch
 
 from guet.config.committer import Committer
+from guet.context.context import Context
 from guet.hooks.post_commit import post_commit
 
 
-@patch('guet.hooks.post_commit.set_committer_as_author')
-@patch('guet.hooks.post_commit.set_current_committers')
+@patch('guet.hooks.post_commit.Context')
 @patch('guet.hooks.post_commit.get_current_committers')
-@patch('guet.hooks.post_commit.Git')
 class TestPostCommit(unittest.TestCase):
 
-    def test_manage_rotates_the_commit_names(self,
-                                             mock_configure_git_author,
-                                             mock_get_committers,
-                                             mock_set_committers,
-                                             mock_set_committer_as_author):
+    def test_sets_committers_to_the_context(self,
+                                            mock_get_committers,
+                                            mock_context):
         committer1 = Committer(name='Name', email='email', initials='')
         committer2 = Committer(name='Name2', email='email', initials='')
-        result = [committer1, committer2]
-
-        mock_get_committers.return_value = result
+        mock_get_committers.return_value = [committer1, committer2]
+        context: Context = mock_context.return_value
 
         post_commit()
 
-        mock_set_committers.assert_called_with([committer2, committer1])
-
-    def test_manage_sets_the_new_author_name_and_email(self,
-                                                       mock_configure_git_author,
-                                                       mock_get_committers,
-                                                       mock_set_committers,
-                                                       mock_set_committer_as_author):
-        committer1 = Committer(name='Name', email='email', initials='')
-        committer2 = Committer(name='Name2', email='email', initials='')
-        result = [committer1, committer2]
-        mock_get_committers.return_value = result
-
-        post_commit()
-
-        mock_set_committer_as_author.assert_called_with(committer2)
-
-    def test_manage_configures_git_to_use_new_author(self,
-                                                     mock_git,
-                                                     mock_get_committers,
-                                                     mock_set_committers,
-                                                     mock_set_committer_as_author):
-        committer1 = Committer(name='Name', email='email', initials='')
-        committer2 = Committer(name='Name2', email='email', initials='')
-        result = [committer1, committer2]
-        mock_get_committers.return_value = result
-
-        post_commit()
-
-        git = mock_git()
-        self.assertEqual(git.author.name, committer2.name)
-        self.assertEqual(git.author.email, committer2.email)
+        context.set_committers.assert_called_with([committer2, committer1])
