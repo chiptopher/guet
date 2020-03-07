68c49f3bc55c0f017d389bc21f9a5a7a69d1e18c
chiptopher <chrisboyerdev@gmail.com>
2020-03-07 12:15:35 -0500

    Refactor init command factory to use Context factory

--- test/commands/test_init.py
+++ test/commands/test_init.py
@@ -1,17 +1,18 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from guet.commands.init.factory import InitCommandFactory
 from guet.settings.settings import Settings
 
 
+@patch('guet.commands.command_factory_with_context.Context')
 class TestInitDataSourceCommand(unittest.TestCase):
 
     @patch('guet.commands.init.factory.already_initialized')
     @patch('guet.commands.init.factory.initialize')
     def test_execute_uses_gateway_to_create_data_source(self,
                                                         mock_initialize,
-                                                        mock_already_initialized):
+                                                        mock_already_initialized, _1):
         mock_already_initialized.return_value = False
         command = InitCommandFactory().build(['init'], Settings())
         command.execute()
@@ -21,7 +22,7 @@ class TestInitDataSourceCommand(unittest.TestCase):
     @patch('guet.commands.init.factory.already_initialized')
     def test_execute_prints_out_error_message_when_calling_init_when_it_has_already_been_called(self,
                                                                                                 mock_already_initialized,
-                                                                                                mock_print):
+                                                                                                mock_print, _1):
         mock_already_initialized.return_value = True
 
         command = InitCommandFactory().build(['init'], Settings())
@@ -29,5 +30,5 @@ class TestInitDataSourceCommand(unittest.TestCase):
 
         mock_print.assert_called_once_with('Config folder already exists.')
 
-    def test_get_short_help_message(self):
+    def test_get_short_help_message(self, _1):
         self.assertEqual('Initialize guet for use', InitCommandFactory().short_help_message())
