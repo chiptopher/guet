5cd8cd04154f0d4824f10e83dd24dcd0315c098e
chiptopher <chrisboyerdev@gmail.com>
2020-03-21 17:04:19 -0400

    Add a LocalCommitter that saves to project root

---
+++ test/committers/test_local_committer.py
@@ -0,0 +1,15 @@
+from os.path import join
+from unittest import TestCase
+from unittest.mock import patch
+
+from guet import constants
+from guet.committers.local_committer import LocalCommitter
+
+
+@patch('guet.committers.local_committer.add_committer')
+class TestLocalCommitterSave(TestCase):
+    def test_adds_committer_to_project_root(self, mock_add_committer):
+        committer = LocalCommitter('name', 'email', 'initials', '/path/to/root')
+        committer.save()
+        file_path = join('/path/to/root', '.guet', constants.COMMITTERS)
+        mock_add_committer.assert_called_with('initials', 'name', 'email', file_path=file_path)
