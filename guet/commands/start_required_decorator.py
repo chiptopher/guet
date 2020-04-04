b92242658dbd662263cdd6aa5e693e849dca118d
chiptopher <chrisboyerdev@gmail.com>
2020-04-04 10:34:23 -0400

    StartRequiredDecorator loads git from project root directory

--- guet/commands/start_required_decorator.py
+++ guet/commands/start_required_decorator.py
@@ -1,13 +1,15 @@
+from os.path import join
 from typing import List
+
+from guet.git.errors import NoGitPresentError
 
 from guet.commands.command import Command
 from guet.commands.command_factory_decorator import CommandFactoryDecorator
 from guet.commands.print_strategy import PrintCommandStrategy
 from guet.commands.strategy_command import StrategyCommand
-from guet.git.errors import NoGitPresentError
 from guet.git.git import Git
-from guet.git.git_path_from_cwd import git_path_from_cwd
 from guet.settings.settings import Settings
+from guet.util import project_root
 
 GUET_NOT_STARTED_ERROR = (
     'guet not initialized in this repository. '
@@ -24,10 +26,10 @@ NOT_RAN_IN_ROOT_DIRECTORY_ERROR = (
 class StartRequiredDecorator(CommandFactoryDecorator):
     def build(self, args: List[str], settings: Settings) -> Command:
         try:
-            git = Git(git_path_from_cwd())
-            if git.hooks_present():
-                return self.decorated.build(args, settings)
-            else:
-                return StrategyCommand(PrintCommandStrategy(GUET_NOT_STARTED_ERROR))
-        except NoGitPresentError:
+            git = Git(join(project_root(), '.git'))
+        except FileNotFoundError:
             return StrategyCommand(PrintCommandStrategy(NOT_RAN_IN_ROOT_DIRECTORY_ERROR))
+        if git.hooks_present():
+            return self.decorated.build(args, settings)
+        else:
+            return StrategyCommand(PrintCommandStrategy(GUET_NOT_STARTED_ERROR))
--- test/commands/test_start_required_decorator.py
+++ test/commands/test_start_required_decorator.py
@@ -7,25 +7,25 @@ from guet.git.errors import NoGitPresentError
 from guet.settings.settings import Settings
 
 
-@patch('guet.commands.start_required_decorator.git_path_from_cwd')
+@patch('guet.commands.start_required_decorator.project_root')
 @patch('guet.commands.start_required_decorator.Git')
 class TestStartRequiredDecorator(TestCase):
-    def test_calls_decorated_build(self, mock_git, mock_git_path_from_cwd):
+    def test_calls_decorated_build(self, mock_git, mock_project_root):
         mock_command = Mock()
         mock_factory = Mock()
         mock_factory.build.return_value = mock_command
         decorator = StartRequiredDecorator(mock_factory)
         self.assertEqual(mock_command, decorator.build([], Settings()))
 
-    def test_returns_command_with_print_strategy_with_error_message(self, mock_git, mock_git_path_from_cwd):
+    def test_returns_command_with_print_strategy_with_error_message(self, mock_git, mock_project_root):
         decorator = StartRequiredDecorator(Mock())
         mock_git.return_value.hooks_present.return_value = False
         command = decorator.build([], Settings())
 
         self.assertIsInstance(command.strategy, PrintCommandStrategy)
 
-    def test_returns_command_with_print_strategy_with_error_message(self, mock_git, mock_git_path_from_cwd):
-        mock_git.side_effect = NoGitPresentError()
+    def test_returns_error_message_when_no_git_present(self, mock_git, mock_project_root):
+        mock_git.side_effect = FileNotFoundError()
         decorator = StartRequiredDecorator(Mock())
         command = decorator.build([], Settings())
 
