1032527a779c3f451f0e60022824168d425853d0
chiptopher <chrisboyerdev@gmail.com>
2020-04-04 21:14:38 -0400

    Add project path to set_current_committers

--- guet/config/set_current_committers.py
+++ guet/config/set_current_committers.py
@@ -7,22 +7,21 @@ from guet.config import CONFIGURATION_DIRECTORY
 from guet.config.committer import Committer
 from guet.files.write_lines import write_lines
 from guet.files.read_lines import read_lines
-from guet.git.git_path_from_cwd import git_path_from_cwd
 
 
 def _all_committers_set_from_file() -> List[str]:
     return read_lines(join(CONFIGURATION_DIRECTORY, constants.COMMITTERS_SET))
 
 
-def _format_committers_to_committers_set_format(committers: List[Committer]) -> str:
-    git_path = git_path_from_cwd()
+def _format_committers_to_committers_set_format(committers: List[Committer], project_path: str) -> str:
+    git_path = project_path
     current_time_in_millis = int(round(time.time() * 1000))
     committer_initials = ','.join([committer.initials for committer in committers])
     return committer_initials + f',{current_time_in_millis},{git_path}'
 
 
-def _add_to_current_set_lines(current_set, formatted_set_committers_information):
-    git_path = git_path_from_cwd()
+def _add_to_current_set_lines(current_set, formatted_set_committers_information, project_path: str):
+    git_path = project_path
     line_with_git_path = next((line for line in current_set if line.endswith(git_path)), None)
     if line_with_git_path:
         index = current_set.index(line_with_git_path)
@@ -31,9 +30,9 @@ def _add_to_current_set_lines(current_set, formatted_set_committers_information)
         current_set.append(formatted_set_committers_information)
 
 
-def set_current_committers(committers: List[Committer]) -> None:
+def set_current_committers(committers: List[Committer], project_path: str) -> None:
     path = join(CONFIGURATION_DIRECTORY, constants.COMMITTERS_SET)
     current_set = _all_committers_set_from_file()
-    formatted_set_committers_information = _format_committers_to_committers_set_format(committers)
-    _add_to_current_set_lines(current_set, formatted_set_committers_information)
+    formatted_set_committers_information = _format_committers_to_committers_set_format(committers, project_path)
+    _add_to_current_set_lines(current_set, formatted_set_committers_information, project_path)
     write_lines(path, current_set)
--- test/config/test_set_current_committers.py
+++ test/config/test_set_current_committers.py
@@ -12,60 +12,53 @@ class TestSetCurrentCommitters(unittest.TestCase):
 
     @patch('guet.config.set_current_committers.read_lines')
     @patch('guet.config.set_current_committers.write_lines')
-    @patch('guet.config.set_current_committers.git_path_from_cwd')
     @patch('time.time')
     def test_writes_committer_initials_and_current_time_to_committers_set_file(self,
                                                                                mock_time,
-                                                                               mock_git_path,
                                                                                mock_write_lines,
                                                                                mock_read_lines):
         mock_read_lines.return_value = []
-        mock_git_path.return_value = '/absolute/path/to/.git'
         mock_time.return_value = 1000000000
-        set_current_committers([Committer('name', 'email', 'initials1'), Committer('name', 'email', 'initials2')])
+        set_current_committers([Committer('name', 'email', 'initials1'), Committer('name', 'email', 'initials2')],
+                               '/path/to/project/.git')
 
         mock_write_lines.assert_called_with(join(CONFIGURATION_DIRECTORY, constants.COMMITTERS_SET),
-                                            ['initials1,initials2,1000000000000,/absolute/path/to/.git'])
+                                            ['initials1,initials2,1000000000000,/path/to/project/.git'])
 
     @patch('guet.config.set_current_committers.read_lines')
     @patch('guet.config.set_current_committers.write_lines')
-    @patch('guet.config.set_current_committers.git_path_from_cwd')
     @patch('time.time')
     def test_adds_given_committer_initials_to_committers_set_file(self,
                                                                   mock_time,
-                                                                  mock_git_path,
                                                                   mock_write_lines,
                                                                   mock_read_lines):
         mock_read_lines.return_value = [
             'initials3,initials4,1000000000000,/absolute/path/to/other/.git',
         ]
-        mock_git_path.return_value = '/absolute/path/to/.git'
         mock_time.return_value = 1000000000
-        set_current_committers([Committer('name', 'email', 'initials1'), Committer('name', 'email', 'initials2')])
-
+        set_current_committers([Committer('name', 'email', 'initials1'), Committer('name', 'email', 'initials2')],
+                               '/path/to/project/.git')
         lines = [
             'initials3,initials4,1000000000000,/absolute/path/to/other/.git',
-            'initials1,initials2,1000000000000,/absolute/path/to/.git'
+            'initials1,initials2,1000000000000,/path/to/project/.git'
         ]
         mock_write_lines.assert_called_with(join(CONFIGURATION_DIRECTORY, constants.COMMITTERS_SET), lines)
 
     @patch('guet.config.set_current_committers.read_lines')
     @patch('guet.config.set_current_committers.write_lines')
-    @patch('guet.config.set_current_committers.git_path_from_cwd')
     @patch('time.time')
     def test_overwrites_set_initials_if_git_path_matches(self,
                                                          mock_time,
-                                                         mock_git_path,
                                                          mock_write_lines,
                                                          mock_read_lines):
         mock_read_lines.return_value = [
-            'initials1,initials2,1000000,/absolute/path/to/.git',
+            'initials1,initials2,1000000,/path/to/project/.git',
         ]
-        mock_git_path.return_value = '/absolute/path/to/.git'
         mock_time.return_value = 1000000000
-        set_current_committers([Committer('name', 'email', 'initials3'), Committer('name', 'email', 'initials4')])
+        set_current_committers([Committer('name', 'email', 'initials3'), Committer('name', 'email', 'initials4')],
+                               '/path/to/project/.git')
 
         lines = [
-            'initials3,initials4,1000000000000,/absolute/path/to/.git'
+            'initials3,initials4,1000000000000,/path/to/project/.git'
         ]
         mock_write_lines.assert_called_with(join(CONFIGURATION_DIRECTORY, constants.COMMITTERS_SET), lines)
