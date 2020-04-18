fd5ebf4b82312eabca2cc5f27f6211080b9e7f45
chiptopher <chrisboyerdev@gmail.com>
2020-04-18 13:11:55 -0400

    Adding committer that is exact match of global committer succeeds

--- e2e/test_addcommitter.py
+++ e2e/test_addcommitter.py
@@ -80,13 +80,18 @@ class TestAddUser(DockerTest):
 
     def test_prints_warning_message_when_adding_local_committer_that_has_same_initials_as_global_committer(self):
         self.guet_init()
+        self.git_init()
+        self.guet_start()
         self.guet_add('initials', 'name1', 'email1')
-        self.guet_add('initials', 'name2', 'emial2', local=True)
+        self.guet_add('initials', 'name2', 'email2', local=True)
+        self.save_file_content('test-env/.guet/committers')
 
         self.execute()
 
-        self.assert_text_in_logs(0, ('Adding committer with initials "initials" shadows the '
+        self.assert_text_in_logs(1, ('Adding committer with initials "initials" shadows the '
                                      'global committer "initials" - "name1" <email1>'))
+        text = self.get_file_text('test-env/.guet/committers')
+        self.assertListEqual(['initials,name2,email2'], text)
 
     def test_adding_local_committer_in_subfolder_adds_committer_to_root_directory(self):
         self.guet_init()
