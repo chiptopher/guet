9d42708d89c0c3bee4b42433d286c17a4a3e4753
chiptopher <chrisboyerdev@gmail.com>
2020-04-23 07:39:43 -0400

    Remove git_path_from_cwd and use context.git in start factory

--- guet/commands/usercommands/start/factory.py
+++ guet/commands/usercommands/start/factory.py
@@ -7,8 +7,6 @@ from guet.commands.usercommands.start.create_hook_strategy import CreateHookStra
 from guet.commands.usercommands.start.start_strategy import PromptUserForHookTypeStrategy
 from guet.commands.strategies.strategy_command import StrategyCommand
 from guet.commands.usercommands.usercommand_factory import UserCommandFactory
-from guet.git.git import Git
-from guet.git.git_path_from_cwd import git_path_from_cwd
 from guet.settings.settings import Settings
 
 START_HELP_MESSAGE = HelpMessageBuilder('guet start',
@@ -22,8 +20,8 @@ class StartCommandFactory(UserCommandFactory):
         return 'Start guet usage in the repository at current directory'
 
     def build(self, args: List[str], settings: Settings) -> Command:
-        git_path = git_path_from_cwd().replace('/hooks', '')
-        git = Git(git_path)
+        git = self.context.git
+        git_path = git.path_to_repository
         if '-a' in args or '--alongside' in args:
             strategy = CreateAlongsideHookStrategy(git_path, self.context)
         elif '-o' in args or '--overwrite' in args:
--- guet/git/git_path_from_cwd.py
+++
@@ -1,6 +0,0 @@
-from os import getcwd
-from os.path import join
-
-
-def git_path_from_cwd():
-    return join(getcwd(), '.git')
