edc42eedb962743077b262293e182a43ba5cc260
chiptopher <chrisboyerdev@gmail.com>
2020-05-02 10:06:27 -0400

    Print a better prompt when attempting to start in a repo that alread has hooks

--- guet/commands/usercommands/start/start_strategy.py
+++ guet/commands/usercommands/start/start_strategy.py
@@ -6,9 +6,11 @@ from guet.commands.usercommands.start.hook_strategy import HookStrategy
 
 class PromptUserForHookTypeStrategy(HookStrategy):
     def apply(self):
-        print(('There is already commit hooks in this project. ' +
-               'Would you like to overwrite (o), ' +
-               'create (a) the file and put it in the hooks folder, or cancel (x)?'))
+        print('There is already commit hooks in this project. You can')
+        print('  (o) overwrite current hooks. This will delete any matching hooks.')
+        print(('  (a) create guet hooks alongside current ones.'
+               'This will create them with \'-guet\' appended on the name of the hook file.'))
+        print('  (x) cancel the request. This will do nothing.')
         val = input()
         if val == 'o':
             CreateHookStrategy(self.git).apply()
