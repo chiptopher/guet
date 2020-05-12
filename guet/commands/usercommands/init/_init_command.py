f06e4e8d75859f69b0240d2fd539e537c43ed99c
chiptopher <chrisboyerdev@gmail.com>
2020-05-11 20:13:08 -0400

    Print error message when initializing more than once

--- guet/commands/usercommands/init/_init_command.py
+++ guet/commands/usercommands/init/_init_command.py
@@ -1,6 +1,10 @@
 from guet.commands._context_command import ContextCommand
+from guet.config.errors import AlreadyInitializedError
 
 
 class InitCommand(ContextCommand):
     def execute(self):
-        self._context.initialize()
+        try:
+            self._context.initialize()
+        except AlreadyInitializedError:
+            print('Config folder already exists.')
