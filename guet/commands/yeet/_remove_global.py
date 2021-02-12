92407bc7e4d27ba2be41b250d91fb8ba21139ada
chiptopher <chrisboyerdev@gmail.com>
2021-02-12 13:33:44 -0500

    Add force flag to guet yeet to remove global guet config

---
+++ guet/commands/yeet/_remove_global.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from shutil import rmtree
+from typing import List
+
+from guet.config import CONFIGURATION_DIRECTORY
+from guet.steps.action import Action
+
+
+class RemoveGlobal(Action):
+    def execute(self, args: List[str]):
+        if any(arg in ('-f', '--force') for arg in args):
+            configuration_dir = Path(CONFIGURATION_DIRECTORY)
+            rmtree(configuration_dir)
+            print('Bye!')
