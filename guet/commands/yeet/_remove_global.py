87519cc1d15dc9d5c7a8f87bde2ae7bc733c4ae7
chiptopher <chrisboyerdev@gmail.com>
2021-05-10 07:08:44 -0400

    Rename --force to --global

--- guet/commands/yeet/_remove_global.py
+++ guet/commands/yeet/_remove_global.py
@@ -8,7 +8,7 @@ from guet.steps.action import Action
 
 class RemoveGlobal(Action):
     def execute(self, args: List[str]):
-        if any(arg in ('-f', '--force') for arg in args):
+        if any(arg in ('-g', '--global') for arg in args):
             configuration_dir = Path(CONFIGURATION_DIRECTORY)
             rmtree(configuration_dir)
             print('Bye!')
