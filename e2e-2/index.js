7bd0662819f8137eb3f037a07cc720348f0d7407
chiptopher <chrisboyerdev@gmail.com>
2022-08-12 13:26:47 -0400

    Make the tests possible to run in parallel

--- e2e-2/index.js
+++ e2e-2/index.js
@@ -1,18 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { execSync } = require('child_process');
 
+const colors = require('colors');
 const Docker = require('dockerode');
 
 const output = execSync(`npx jest e2e-2 --listTests`);
 
+colors.enable();
 async function main() {
     let failed = false;
 
     let filterText;
 
+    let parallel = false;
+
     if (process.argv.includes('--filter')) {
         const index = process.argv.indexOf('--filter') + 1;
         filterText = process.argv[index];
+    }
+
+    if (process.argv.includes('--parallel')) {
+        parallel = true;
     }
 
     const testFiles = String(output)
@@ -26,29 +34,57 @@ async function main() {
             }
         });
 
-    for (let i = 0; i < testFiles.length; i++) {
-        const testClient = new Docker();
-        const runCommand = ['npx', 'jest', testFiles[i].split('guet/')[1]];
-        const result = await testClient.run(
-            'guettest:0.0.1',
-            runCommand,
-            process.stdout,
-            {
-                rm: true,
-            }
-        );
+    failed = await (parallel
+        ? runInParallel(testFiles)
+        : runSequentially(testFiles));
 
+    if (failed) {
+        process.exit(1);
+    }
+}
+
+async function runSequentially(testFiles) {
+    let failed = false;
+    for (let i = 0; i < testFiles.length; i++) {
+        const result = await runForFile(testFiles[i], process.stdout);
         const exitCode = result[0]['StatusCode'];
-        result[1].remove();
 
         if (exitCode > 0) {
             failed = true;
         }
     }
+    return failed;
+}
+
+async function runInParallel(testFiles) {
+    console.log('Running in parallel'.green);
+    const failed = Boolean(
+        await Promise.all(
+            testFiles.map(file => runForFile(file, undefined))
+        ).then(results =>
+            results
+                .map(result => {
+                    const exitCode = result[0]['StatusCode'];
+                    return exitCode;
+                })
+                .reduce((a, b) => a + b, 0)
+        )
+    );
 
     if (failed) {
-        process.exit(1);
+        console.log('Encountered an error'.red);
     }
+
+    return failed;
+}
+
+async function runForFile(fileName, out) {
+    const testClient = new Docker();
+    const runCommand = ['npx', 'jest', fileName.split('guet/')[1]];
+    const result = await testClient.run('guettest:0.0.1', runCommand, out);
+
+    result[1].remove();
+    return result;
 }
 
 main().catch(console.error);
