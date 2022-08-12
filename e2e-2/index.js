a4f1ecc4e5675a9d57e1de4d267b0264a5ed3adf
chiptopher <chrisboyerdev@gmail.com>
2022-08-12 09:40:38 -0400

    Make the test runnner run in band

--- e2e-2/index.js
+++ e2e-2/index.js
@@ -4,24 +4,32 @@ const { execSync } = require('child_process');
 const Docker = require('dockerode');
 
 const output = execSync(`npx jest e2e-2 --listTests`);
-Promise.all(
-    String(output)
-        .split('\n')
-        .filter(line => line !== '')
-        .map(async testPath => {
-            const testClient = new Docker();
-            const runCommand = ['npx', 'jest', testPath.split('guet/')[1]];
-            const result = await testClient.run(
-                'guettest:0.0.1',
-                runCommand,
-                process.stdout
-            );
 
-            const exitCode = result[0]['StatusCode'];
-            return exitCode;
-        })
-).then(exitCodes => {
-    if (exitCodes.find(exitCode => exitCode > 0)) {
+async function main() {
+    let failed = false;
+
+    const testFiles = String(output)
+        .split('\n')
+        .filter(line => line !== '');
+
+    for (let i = 0; i < testFiles.length; i++) {
+        const testClient = new Docker();
+        const runCommand = ['npx', 'jest', testFiles[i].split('guet/')[1]];
+        const result = await testClient.run(
+            'guettest:0.0.1',
+            runCommand,
+            process.stdout
+        );
+
+        const exitCode = result[0]['StatusCode'];
+        if (exitCode > 0) {
+            failed = true;
+        }
+    }
+
+    if (failed) {
         process.exit(1);
     }
-});
+}
+
+main().catch(console.error);
